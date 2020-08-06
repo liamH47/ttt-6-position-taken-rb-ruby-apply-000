8c6e2666e75b9de1a59c4155eb452985cdd41dcd
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-06 18:08:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,6 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X"
-    return true 
-  elsif board[index] == "O"
-    return true
+
   end
 end
